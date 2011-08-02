78f9c0d4c61220cc38e98b170a685796a31dc709
Adam Strauch <cx@initd.cz>
2011-08-03 01:17:26 +0200

    Setup.py fixies

--- setup.py
+++ setup.py
@@ -8,15 +8,15 @@ def read(fname):
 
 setup(
     name = "subtitler",
-    version = "0.1",
+    version = "0.1.1",
     author = "Adam Strauch",
     author_email = "cx@initd.cz",
-    description = ("Tool for searching subtitles by hash or typed query."),
+    description = ("Tool/library for searching subtitles by hash or typed query."),
     license = "BSD",
     keywords = "subtitle",
-    url = "",
+    url = "https://github.com/creckx/Subtitler",
     packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
-    long_description=read('README'),
+    long_description="Subtitler is small tool for downloading subtitles from opensubtitles.org. Contain simple library for XML-RPC API.",#read('README'),
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Topic :: Utilities",
